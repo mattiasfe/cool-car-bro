16492069a3421919b43b0154788a39dd9db9ce1f
noname0000-blip <augusteide98@hotmail.com>
2025-03-06 10:29:58 +0100

    Create mainView.js

---
+++ mainView.js
@@ -0,0 +1,8 @@
+function updateMainView() {
+    let html = `
+    ${wantToStartTimer ? `${renderTimerView()}` : ''}
+    ${updateStatus()}
+    ${updateCarView()}
+    `
+    document.getElementById('app').innerHTML = html;
+}
